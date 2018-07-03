d86b911d2ba9e7cfb2dd2b0d23bff56ecf0e7c35
Taynan Souza <taynan.tws@gmail.com>
2018-07-03 15:07:02 -0300

    first commit

---
+++ Assets/WM2000/Hacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ShowMainMenu();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void ShowMainMenu() {
+		Terminal.ClearScreen();
+		Terminal.WriteLine("What would you lik to hack into? \n");
+		Terminal.WriteLine("Press 1 for the local library");
+		Terminal.WriteLine("Press 2 for the police station");
+		Terminal.WriteLine("Press 3 for the NASA \n");
+		Terminal.WriteLine("Enter your selection: ");
+	}
+}
