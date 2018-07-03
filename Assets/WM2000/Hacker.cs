83820a5404cef93363f8baff907a2ef5ad569809
Taynan Souza <taynan.tws@gmail.com>
2018-07-03 16:25:01 -0300

    Section 15 - 21

--- Assets/WM2000/Hacker.cs
+++ Assets/WM2000/Hacker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+	int level;
 
 	// Use this for initialization
 	void Start () {
@@ -10,12 +11,7 @@ public class Hacker : MonoBehaviour {
 		ShowMainMenu(greeting);
 	}
 
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	void ShowMainMenu(string greeting) {
+	void ShowMainMenu(string greeting = "Hello") {
 		Terminal.ClearScreen();
 		Terminal.WriteLine(greeting);
 		Terminal.WriteLine("What would you lik to hack into? \n");
@@ -23,5 +19,28 @@ public class Hacker : MonoBehaviour {
 		Terminal.WriteLine("Press 2 for the police station");
 		Terminal.WriteLine("Press 3 for the NASA \n");
 		Terminal.WriteLine("Enter your selection: ");
+	}
+
+	void OnUserInput(string input) {
+		print(input);
+		print(input == "1");
+
+		if(input == "1") {
+			level = 1;
+			StartGame();
+		} else if (input == "2") {
+			level = 2;
+			StartGame();
+		} else if(input == "42") {
+			Terminal.WriteLine("a resposta para a vida o universo e tudo mais");
+		} else if(input == "menu") {
+			ShowMainMenu();
+		} else {
+			Terminal.WriteLine("Please, enter a valid option or type menu:");
+		}
+	}
+
+	void StartGame() {
+		Terminal.WriteLine("You chose level " + level);
 	}
 }
